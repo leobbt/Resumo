314d02013d636bb79c62a6b7590989a962f25e3c
Leonardo B Brentano <95104325+leobbt@users.noreply.github.com>
2022-05-17 15:51:21 -0300

    Add files via upload

---
+++ Revisão.js
@@ -0,0 +1,46 @@
+/* 
+    Pong:
+    - createSprite
+    - Background Colors
+    - Edges
+    - bounceOff
+    - velocity
+    - if (condicao){}
+    - keyDown
+    - function
+
+    Trex:
+    - Preload
+    - addImage
+    - addAnimation
+    - changeAnimation
+    - Background infinito
+    - random
+    - math.round
+    - frameCount
+
+    AngryBirds:
+    - Class
+    - constructor
+        - mecanismos de Fisica
+    - display
+        - color
+        - shape
+    - matter
+    - body
+    - this
+    - world
+    - matriz
+
+    RacingCars:
+    - firebase
+    - 2 players
+    - API e JSON
+    - Classes de jogo
+    - Camera 
+    - windowWith
+    - windowHeight
+    - Form
+    - HTML
+
+*/
